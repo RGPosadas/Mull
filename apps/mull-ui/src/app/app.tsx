b6432b04d7a3bd4736bb686b7f2e5dba9da6dce0
Jonathan Hsu <bit172@gmail.com>
2020-10-04 18:19:44 -0400

    Add Create Event page #27
    Co-authored-by: JimLau49 <jimmy71471@gmail.com>

--- apps/mull-ui/src/app/app.tsx
+++ apps/mull-ui/src/app/app.tsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Message } from '@mull/types';
 import { environment } from '../environments/environment';
 
+<<<<<<< HEAD
 import CreateEventPage from './pages/create-event/create-event';
+=======
+import CreateEventPage from './pages/create-event/create-event.component';
+>>>>>>> 1f180a8... Add Create Event page #27
 
 import NavigationBar from './components/navigation-bar/navigation-bar';
 import { Redirect, Route, Switch } from 'react-router-dom';
@@ -20,7 +24,11 @@ export const App = () => {
           <Route exact path="/">
             {<Redirect to={ROUTES.HOME} />}
           </Route>
+<<<<<<< HEAD
           <Route exact path={ROUTES.CREATE_EVENT} component={CreateEventPage} />
+=======
+          <Route exact path="/create-event" component={CreateEventPage} />
+>>>>>>> 1f180a8... Add Create Event page #27
         </Switch>
       </div>
       <NavigationBar />
