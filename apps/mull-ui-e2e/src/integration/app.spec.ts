350cb2be950199e95dade7e8f0b0634851d2a57c
Cristian Aldea <aldea.cristian@outlook.com>
2020-10-07 22:17:35 -0400

    Improve styling and compatibility with older browsers #35
    the gap property is relatively new, and not support on some older versions of browsers. It was removed in favour of using margins.
    
    Co-authored-by: Leo <leojrsilao@gmail.com>

--- apps/mull-ui-e2e/src/integration/app.spec.ts
+++ apps/mull-ui-e2e/src/integration/app.spec.ts
@@ -1,5 +1,3 @@
-import { getGreeting } from '../support/app.po';
-
 describe('mull-ui', () => {
   beforeEach(() => cy.visit('/'));
 });
--- apps/mull-ui/src/app/components/navigation-bar/navigation-bar.tsx
+++ apps/mull-ui/src/app/components/navigation-bar/navigation-bar.tsx
@@ -23,19 +23,19 @@ export const NavigationBar = () => {
     <div className="nav-container">
       <img src={logo} className="logo nav-element" alt="Mull logo" />
       <NavLink to={ROUTES.HOME} activeClassName="active" data-testid="home-navlink">
-        <HomeIcon className="nav-button" />
+        <HomeIcon className="nav-button mobile-button" />
       </NavLink>
       <NavLink to={ROUTES.MAP} activeClassName="active" data-testid="map-navlink">
-        <MapIcon className="nav-button" />
+        <MapIcon className="nav-button mobile-button" />
       </NavLink>
       <NavLink to={ROUTES.CREATE_EVENT} activeClassName="active" data-testid="new-event-navlink">
-        <NewEventIcon className="nav-middle-button" />
+        <NewEventIcon className="nav-button mobile-middle-button" />
       </NavLink>
       <NavLink to={ROUTES.TOOLS} activeClassName="active" data-testid="tools-navlink">
-        <ToolMenuIcon className="nav-button" />
+        <ToolMenuIcon className="nav-button mobile-button" />
       </NavLink>
       <NavLink to={ROUTES.PROFILE} activeClassName="active" data-testid="profile-navlink">
-        <ProfileIcon className="nav-button" />
+        <ProfileIcon className="nav-button mobile-button" />
       </NavLink>
     </div>
   );
