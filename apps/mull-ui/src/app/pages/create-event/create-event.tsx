2d8802963552cadee244513ebd55a90533376e05
Jonathan Hsu <bit172@gmail.com>
2020-10-17 18:33:14 -0400

    Add formik and Yup to create event form #27
    Co-authored-by: Jimmy Lau <jimmy71471@gmail.com>

--- apps/mull-ui/src/app/pages/create-event/create-event.tsx
+++ apps/mull-ui/src/app/pages/create-event/create-event.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { PillOptions } from '@mull/ui-lib';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 import DateCalendar from '../create-event/date-calendar/date-calendar';
 
 import './create-event.scss';
@@ -8,87 +11,146 @@ import './create-event.scss';
 export interface CreateEventProps {}
 
 const CreateEventPage = () => {
+  const formik = useFormik({
+    initialValues: {
+      activeRestriction: 0,
+      startDate: new Date(),
+      endDate: new Date(),
+      startTime: '',
+      endTime: '',
+      eventTitle: '',
+      description: '',
+      location: '',
+    },
+
+    validationSchema: Yup.object({
+      startDate: Yup.date(),
+      endDate: Yup.date(),
+      startTime: Yup.string(),
+      endTime: Yup.string(),
+      eventTitle: Yup.string()
+        .required('Event Title is required.')
+        .length(65, 'Event Title length must be under 65 characters.'),
+      activeRestriction: Yup.number().min(0).max(2),
+      description: Yup.string()
+        .required('Event Description is required.')
+        .length(5000, 'Event Description must be under 5000 characters'),
+      location: Yup.string().required('Event Location is required.'),
+    }),
+
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   const [imageFile, setImageFile] = useState(null);
-  const [activeRestriction, setActiveRestriction] = useState(null);
-  const [startTime, setStartTime] = useState(null);
-  const [endTime, setEndTime] = useState(null);
 
   const handleFileUpload = (event) => {
-    console.log(event.target.files[0]);
     setImageFile(URL.createObjectURL(event.target.files[0]));
   };
 
   const handleRestrictions = (idx) => {
-    setActiveRestriction(idx);
-  };
-
-  const handleStartTime = (event) => {
-    console.log(event.target.value);
-    setStartTime(event.target.value);
-  };
-
-  const handleEndTime = (event) => {
-    console.log(event.target.value);
-    setEndTime(event.target.value);
+    formik.setFieldValue('activeRestriction', idx);
   };
 
   return (
-    <div className="create-event">
-      <p className="create-event-text" style={{ marginTop: '120px' }}>
-        Create Event
-      </p>
-      <label htmlFor="file-upload" className="custom-file-upload event-input-border">
-        {imageFile ? (
-          <img src={imageFile} style={{ width: '50%', height: '50%' }} alt="sup" />
-        ) : (
-          '🧼'
-        )}
-      </label>
-      <input
-        className="event-image-upload"
-        onChange={handleFileUpload}
-        id="file-upload"
-        type="file"
-        style={{ marginTop: '120px' }}
-      />
-
-      <DateCalendar />
-
-      <div className="time-picker">
-        <label className="create-event-label">Start Time</label>
-
-        <input type="time" className="event-time" onChange={handleStartTime} />
-      </div>
-      <div className="time-picker">
-        <label className="create-event-label">End Time</label>
-
-        <input type="time" className="event-time" onChange={handleEndTime} />
-      </div>
-      <div>
-        <label className="create-event-label" htmlFor="event-title">
-          Event Title
+    <form onSubmit={formik.handleSubmit}>
+      <div className="create-event page-container">
+        <p className="create-event-text">Create Event</p>
+        <label htmlFor="imageFile" className="custom-file-upload event-input-border">
+          {imageFile ? (
+            <img src={imageFile} style={{ width: '50%', height: '50%' }} alt="sup" />
+          ) : (
+            '🧼'
+          )}
         </label>
-        <input className="create-event-input event-input-border" id="event-title" type="text" />
+        <input
+          className="event-image-upload"
+          id="imageFile"
+          type="file"
+          onChange={handleFileUpload}
+        />
+        <DateCalendar
+          startDate={formik.values.startDate}
+          endDate={formik.values.endDate}
+          onStartDateChange={(date) => {
+            formik.setFieldValue('startDate', date);
+          }}
+          onEndDateChange={(date) => {
+            formik.setFieldValue('endDate', date);
+          }}
+        />
+        <div className="time-picker">
+          <label className="create-event-label">Start Time</label>
+          <input
+            type="time"
+            className="event-time"
+            id="startTime"
+            name="startTime"
+            value={formik.values.startTime}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div className="time-picker">
+          <label className="create-event-label">End Time</label>
+          <input
+            type="time"
+            className="event-time"
+            id="endTime"
+            name="endTime"
+            value={formik.values.endTime}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div>
+          <label className="create-event-label" htmlFor="eventTitle">
+            Event Title
+          </label>
+          <input
+            className="create-event-input event-input-border"
+            id="eventTitle"
+            name="eventTitle"
+            type="text"
+            value={formik.values.eventTitle}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div>
+          <label className="create-event-label" htmlFor="description">
+            Description
+          </label>
+          <input
+            className="create-event-input event-input-border"
+            id="description"
+            name="description"
+            type="text"
+            value={formik.values.description}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div>
+          <label className="create-event-label" htmlFor="location">
+            Location
+          </label>
+          <input
+            className="create-event-input event-input-border"
+            id="location"
+            name="location"
+            type="text"
+            value={formik.values.location}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <PillOptions
+          options={['Everyone', 'Friends', 'Invite Only']}
+          onChange={handleRestrictions}
+          active={formik.values.activeRestriction}
+        />
+        <button type="submit" className="create-event-button">
+          create +
+        </button>
       </div>
-      <div>
-        <label className="create-event-label" htmlFor="description">
-          Description
-        </label>
-        <input className="create-event-input event-input-border" id="description" type="text" />
-      </div>
-      <div>
-        <label className="create-event-label" htmlFor="location">
-          Location
-        </label>
-        <input className="create-event-input event-input-border" id="location" type="text" />
-      </div>
-      <PillOptions
-        options={['Everyone', 'Friends', 'Invite Only']}
-        onChange={handleRestrictions}
-        active={activeRestriction}
-      />
-      <button className="create-event-button">create +</button>
-    </div>
+    </form>
   );
 };
 
--- apps/mull-ui/src/app/pages/create-event/date-calendar/date-calendar.tsx
+++ apps/mull-ui/src/app/pages/create-event/date-calendar/date-calendar.tsx
@@ -1,37 +1,41 @@
-import React, { useState } from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import { format } from 'date-fns';
 import { enGB } from 'date-fns/locale';
 import { DateRangeFocus, DateRangePickerCalendar } from 'react-nice-dates';
 import 'react-nice-dates/build/style.css';
 import './date-calendar.scss';
 
-const DateCalendar = () => {
-  let START_DATE: DateRangeFocus = 'startDate';
-  const [startDate, setStartDate] = useState<Date>();
-  const [endDate, setEndDate] = useState<Date>();
+/* eslint-disable-next-line */
+export interface DateCalendarProps {
+  startDate: Date;
+  endDate: Date;
+  onStartDateChange: (date: Date) => void;
+  onEndDateChange: (date: Date) => void;
+}
+
+const DateCalendar = ({
+  startDate,
+  endDate,
+  onStartDateChange,
+  onEndDateChange,
+}: DateCalendarProps) => {
+  const START_DATE: DateRangeFocus = 'startDate';
   const [focus, setFocus] = useState<DateRangeFocus>(START_DATE);
   const handleFocusChange = (newFocus) => {
     setFocus(newFocus || START_DATE);
   };
   return (
     <div>
-      <p>
-        Selected start date:{' '}
-        {startDate ? format(startDate, 'dd MMM yyyy', { locale: enGB }) : 'none'}.
-      </p>
-      <p>
-        Selected end date: {endDate ? format(endDate, 'dd MMM yyyy', { locale: enGB }) : 'none'}.
-      </p>
-      <p>Currently selecting: {focus}.</p>
       <div className="event-calendar">
         <DateRangePickerCalendar
           startDate={startDate}
           endDate={endDate}
           focus={focus}
-          onStartDateChange={(event) => setStartDate(event)}
-          onEndDateChange={(event) => setEndDate(event)}
+          onStartDateChange={onStartDateChange}
+          onEndDateChange={onEndDateChange}
           onFocusChange={handleFocusChange}
           locale={enGB}
+          minimumDate={new Date()}
         />
       </div>
     </div>
