ab9172c3b8d4bf896f9f7a03b6623ebfc0d09344
omar <osahtout@hotmail.com>
2020-10-19 14:35:04 -0400

    removed old entities

--- apps/mull-api/src/app/entities/entities.module.ts
+++ apps/mull-api/src/app/entities/entities.module.ts
@@ -6,8 +6,6 @@ import { Channel } from './channel.entity';
 import { Event } from './event.entity';
 import { Location } from './location.entity';
 import { Media } from './media.entity';
-import { Participants } from './participants.entity';
-import { PostMedia } from './post-media.entity';
 import { PostReaction } from './post-reaction.entity';
 import { Post } from './post.entity';
 import { User } from './user.entity';
@@ -16,19 +14,7 @@ import { User } from './user.entity';
  * Encapsulates all entities of the application.
  */
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      Channel,
-      Event,
-      Location,
-      Media,
-      Participants,
-      PostMedia,
-      PostReaction,
-      Post,
-      User,
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([Channel, Event, Location, Media, PostReaction, Post, User])],
   exports: [TypeOrmModule],
 })
 export class EntitiesModule {}
