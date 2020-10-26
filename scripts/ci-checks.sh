8b2789ecc82a13f329b88278fc8017d214f61ac0
Cristian Aldea <aldea.cristian@outlook.com>
2020-10-25 20:28:18 -0400

    Add conditional checks to travis build #11

---
+++ scripts/ci-checks.sh
@@ -0,0 +1,19 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+        # Run all checks on everything
+        npm run all-projects -- --target=lint
+        npm run all-projects -- --target=test --codeCoverage
+        npm run all-projects -- --target=e2e --headless
+    else
+        if [ "$TRAVIS_BRANCH" == "develop" ]; then
+            export BASE_BRANCH="master"
+        else
+            export BASE_BRANCH="develop"
+        fi
+        npm run affected:lint -- --base=origin/$BASE_BRANCH
+        npm run affected:test -- --base=origin/$BASE_BRANCH --codeCoverage
+        npm run affected:e2e -- --base=origin/$BASE_BRANCH
+    fi
+else
+    echo "Checks won't run on pull request builds"
+fi
