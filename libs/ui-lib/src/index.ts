4f8ea962bd637c3b950edb7503814f2ed3784b01
Cristian Aldea <aldea.cristian@outlook.com>
2020-11-15 16:59:58 -0500

    Refactor lib folders

--- libs/ui-lib/src/index.ts
+++ libs/ui-lib/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/custom-time-picker/custom-time-picker';
-export * from './lib/custom-text-input/custom-text-input';
-export * from './lib/pill-options/pill-options';
-export * from './lib/ui-lib';
+export * from './components/custom-time-picker/custom-time-picker';
+export * from './components/custom-text-input/custom-text-input';
+export * from './components/pill-options/pill-options';
