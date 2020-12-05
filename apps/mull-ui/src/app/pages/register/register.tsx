43818c1a55faa3b7214460f58dceb7084092aa71
Jonathan Hsu <bit172@gmail.com>
2020-12-04 21:45:55 -0500

    feat: Connect front end registration to backend #30
    Co-authored-by: Jimmy Lau <jimmy71471@gmail.com>
    Co-authored-by: Leo Jr. Silao <leojrsilao@gmail.com>

--- apps/mull-ui/src/app/pages/register/register.tsx
+++ apps/mull-ui/src/app/pages/register/register.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { CustomTextInput } from '../../components';
+import React, { useRef } from 'react';
+import { gql, useMutation } from '@apollo/client';
 import * as Yup from 'yup';
+import { toast, TypeOptions } from 'react-toastify';
 import { useFormik } from 'formik';
 import { Link } from 'react-router-dom';
 import { History } from 'history';
+
+import { CustomTextInput } from '../../components';
+import { RegistrationMethod } from '@mull/types';
+
 import logo from '../../../assets/mull-logo.png';
 
 import './register.scss';
@@ -12,7 +17,40 @@ export interface RegisterProps {
   history: History;
 }
 
+const CREATE_USER = gql`
+  mutation CreateUser($createUserInput: CreateUserInput!) {
+    createUser(createUserInput: $createUserInput) {
+      id
+    }
+  }
+`;
+
 const Register = ({ history }: RegisterProps) => {
+  // Reference Id for the toast
+  const toastId = useRef(null);
+  /**
+   * Notifies the user of account creation
+   */
+  const notifySubmissionToast = () => {
+    toastId.current = toast('Submitting Event...', { autoClose: false });
+  };
+
+  // GraphQL mutation hook to create user
+  const [createUser] = useMutation(CREATE_USER);
+
+  /**
+   * Updates existing toast.
+   * @param {TypeOptions} type Type of the toast
+   * @param {string} message Message to display
+   */
+  const updateSubmissionToast = (type: TypeOptions, message: string) => {
+    toast.update(toastId.current, {
+      type,
+      render: message,
+      autoClose: 3000,
+    });
+  };
+
   const formik = useFormik({
     initialValues: {
       name: '',
@@ -26,8 +64,21 @@ const Register = ({ history }: RegisterProps) => {
       password: Yup.string().required('Password is required.'),
     }),
 
-    onSubmit: (values) => {
-      // noop
+    onSubmit: async (values) => {
+      notifySubmissionToast();
+      const payload = { ...values, registrationMethod: RegistrationMethod.LOCAL };
+      createUser({ variables: { createUserInput: payload } })
+        .then(({ errors }) => {
+          if (errors) {
+            updateSubmissionToast(toast.TYPE.ERROR, 'User Not Created');
+          } else {
+            updateSubmissionToast(toast.TYPE.SUCCESS, 'Registration Successful');
+            history.push('/home');
+          }
+        })
+        .catch(() => {
+          updateSubmissionToast(toast.TYPE.ERROR, 'Fatal Error: User Not Created');
+        });
     },
   });
 
