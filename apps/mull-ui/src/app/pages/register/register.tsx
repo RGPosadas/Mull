8d00da2d58628faf05234c4219791e24a777548f
Jonathan Hsu <bit172@gmail.com>
2020-12-04 22:05:38 -0500

    refactor: Created hook for toasts #30
    The toast is being used a lot and this reduces duplicated code

--- apps/mull-ui/src/app/pages/register/register.tsx
+++ apps/mull-ui/src/app/pages/register/register.tsx
@@ -1,13 +1,14 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { gql, useMutation } from '@apollo/client';
 import * as Yup from 'yup';
-import { toast, TypeOptions } from 'react-toastify';
+import { toast } from 'react-toastify';
 import { useFormik } from 'formik';
 import { Link } from 'react-router-dom';
 import { History } from 'history';
 
 import { CustomTextInput } from '../../components';
 import { RegistrationMethod } from '@mull/types';
+import { useToast } from '../../hooks/useToast';
 
 import logo from '../../../assets/mull-logo.png';
 
@@ -26,30 +27,9 @@ const CREATE_USER = gql`
 `;
 
 const Register = ({ history }: RegisterProps) => {
-  // Reference Id for the toast
-  const toastId = useRef(null);
-  /**
-   * Notifies the user of account creation
-   */
-  const notifySubmissionToast = () => {
-    toastId.current = toast('Submitting Event...', { autoClose: false });
-  };
-
   // GraphQL mutation hook to create user
   const [createUser] = useMutation(CREATE_USER);
-
-  /**
-   * Updates existing toast.
-   * @param {TypeOptions} type Type of the toast
-   * @param {string} message Message to display
-   */
-  const updateSubmissionToast = (type: TypeOptions, message: string) => {
-    toast.update(toastId.current, {
-      type,
-      render: message,
-      autoClose: 3000,
-    });
-  };
+  const { notifyToast, updateToast } = useToast();
 
   const formik = useFormik({
     initialValues: {
@@ -65,19 +45,19 @@ const Register = ({ history }: RegisterProps) => {
     }),
 
     onSubmit: async (values) => {
-      notifySubmissionToast();
+      notifyToast('Registering...');
       const payload = { ...values, registrationMethod: RegistrationMethod.LOCAL };
       createUser({ variables: { createUserInput: payload } })
         .then(({ errors }) => {
           if (errors) {
-            updateSubmissionToast(toast.TYPE.ERROR, 'User Not Created');
+            updateToast(toast.TYPE.ERROR, 'User Not Created');
           } else {
-            updateSubmissionToast(toast.TYPE.SUCCESS, 'Registration Successful');
+            updateToast(toast.TYPE.SUCCESS, 'Registration Successful');
             history.push('/home');
           }
         })
         .catch(() => {
-          updateSubmissionToast(toast.TYPE.ERROR, 'Fatal Error: User Not Created');
+          updateToast(toast.TYPE.ERROR, 'Fatal Error: User Not Created');
         });
     },
   });
