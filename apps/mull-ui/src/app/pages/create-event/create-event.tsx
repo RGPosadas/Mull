27c7c220ba13c2eb5ea6c12088ea269f84162414
Jonathan Hsu <bit172@gmail.com>
2020-12-08 18:22:36 -0500

    chore: Remove unused imports #30

--- apps/mull-ui/src/app/pages/create-event/create-event.tsx
+++ apps/mull-ui/src/app/pages/create-event/create-event.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useRef, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { useMutation, gql } from '@apollo/client';
 import { useFormik } from 'formik';
 import { toast } from 'react-toastify';
