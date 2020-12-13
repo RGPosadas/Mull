b7513e406bb4601b4fe18f586dd7481467237195
Ragith Saba <ragith.coolkid@gmail.com>
2020-12-12 22:00:16 -0500

    feat(mull-ui): add discover page component with mock data #76

---
+++ apps/mull-ui/src/app/pages/discover/discover.tsx
@@ -0,0 +1,16 @@
+import { IEvent } from '@mull/types';
+import React from 'react';
+import { EventCard } from '../../components';
+import { dummyEvent } from 'apps/mull-ui/src/constants';
+
+import './discover.scss';
+
+export const DiscoverPage = () => {
+  const events: [IEvent] = Array(5).fill(dummyEvent) as [IEvent]; // will be replaced with a query call
+  const mapped = events.map((event, index) => (
+    <EventCard key={index} event={event} style={{ marginBottom: '2rem' }} />
+  ));
+  return <div className="discover-page-container">{mapped}</div>;
+};
+
+export default DiscoverPage;
