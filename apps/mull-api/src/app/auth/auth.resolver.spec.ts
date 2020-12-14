c5142aa852e180d6fce4e7417bb447cfe9cddd12
Jonathan Hsu <bit172@gmail.com>
2020-12-13 22:36:12 -0500

    feat(mull-api): add test for auth resolver #111

--- apps/mull-api/src/app/auth/auth.resolver.spec.ts
+++ apps/mull-api/src/app/auth/auth.resolver.spec.ts
@@ -1,12 +1,28 @@
+import { GqlContext } from '@mull/types';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthResolver } from './auth.resolver';
+import { Response, Request } from 'express';
+import { AuthResolver, LoginResult } from './auth.resolver';
+import { AuthService } from './auth.service';
+import { LoginInput } from './inputs/auth.input';
+
+const mockAuthService = () => ({
+  validateUser: jest.fn(),
+  createRefreshToken: jest.fn(),
+  sendRefreshToken: jest.fn(),
+  createAccessToken: jest.fn(() => 'accessToken'),
+});
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthResolver],
+      providers: [
+        AuthResolver,
+        {
+          provide: AuthService,
+          useFactory: mockAuthService,
+        },
+      ],
     }).compile();
 
     resolver = module.get<AuthResolver>(AuthResolver);
@@ -14,5 +30,16 @@ describe('AuthResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should login a valid user', async () => {
+    const mockRequest = ({} as unknown) as Request;
+    const mockResponse = ({} as unknown) as Response;
+    const mockGqlContext: GqlContext = { req: mockRequest, res: mockResponse };
+    const result: LoginResult = await resolver.login(
+      { email: 'asd@ad.ca', password: 'abc123' } as LoginInput,
+      mockGqlContext
+    );
+    expect(result.accessToken).toBe('accessToken');
   });
 });
