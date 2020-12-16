fa376de03389bef6fb2656df4f7e98e2dd10c84e
Jonathan Hsu <bit172@gmail.com>
2020-12-15 22:20:09 -0500

    feat(mull-api): remove local strategy #111
    Local authentication is handled by our logic. Therefore, passport will not be used for this.

--- apps/mull-api/src/app/auth/auth.module.ts
+++ apps/mull-api/src/app/auth/auth.module.ts
@@ -7,21 +7,13 @@ import { AuthController } from './auth.controller';
 
 import { AuthResolver } from './auth.resolver';
 
-import { LocalStrategy } from './strategies/local.strategy';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { FacebookStrategy } from './strategies/facebook.strategy';
 import { TwitterStrategy } from './strategies/twitter.strategy';
 
 @Module({
   imports: [UserModule, PassportModule, JwtModule.register({})],
-  providers: [
-    AuthService,
-    LocalStrategy,
-    GoogleStrategy,
-    FacebookStrategy,
-    TwitterStrategy,
-    AuthResolver,
-  ],
+  providers: [AuthService, GoogleStrategy, FacebookStrategy, TwitterStrategy, AuthResolver],
   exports: [AuthService],
   controllers: [AuthController],
 })
