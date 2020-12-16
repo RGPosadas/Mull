1f9a423b48c73f3fa6eebc2c900b3323495cf816
JimLau49 <jimmy71471@gmail.com>
2020-12-15 21:26:40 -0500

    test(mull-api): autocomplete resolver test #29
    Co-authored-by: Yunshi Lin <dabris16.x@gmail.com>

--- apps/mull-api/src/app/event/location-autocomplete/location-autocomplete.resolver.spec.ts
+++ apps/mull-api/src/app/event/location-autocomplete/location-autocomplete.resolver.spec.ts
@@ -1,8 +1,38 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { LocationAutocompleteResolver } from './location-autocomplete.resolver';
+import { Test, TestingModule } from '@nestjs/testing';
+import axiosInstance from '../../../../axiosConfig';
 
-describe('getAutocompletedLocations', () => {
+const userInput = '845 rue Sherbrooke';
+
+jest.mock('../../../../axiosConfig', () => {
+  return {
+    baseURL: `https://app.geocodeapi.io/api/v1/autocomplete?apikey=${process.env.GEOCODE_KEY}&text=test_location&size=5`,
+    request: jest.fn().mockResolvedValue({
+      data: { features: [{ properties: { label: 'mockedLocation' } }] },
+    }),
+  };
+});
+
+describe('autocomplete resolver', () => {
   let resolver: LocationAutocompleteResolver;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LocationAutocompleteResolver],
+    }).compile();
 
-  it('should return an array of autocompleted location', async () => {});
+    resolver = module.get<LocationAutocompleteResolver>(LocationAutocompleteResolver);
+  });
+
+  afterEach(() => jest.resetAllMocks());
+
+  it('should return array of autocomplete locations', async () => {
+    const res = await resolver.getAutocompletedLocations(userInput);
+    expect(axiosInstance.request).toHaveBeenCalled();
+    expect(axiosInstance.request).toHaveBeenCalledWith({
+      method: 'get',
+      url: `https://app.geocodeapi.io/api/v1/autocomplete?apikey=${process.env.GEOCODE_KEY}&text=${userInput}&size=5`,
+    });
+
+    expect(res).toEqual(['mockedLocation']);
+  });
 });
