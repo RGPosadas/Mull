e38f45bc2f7eafa1bc612739ac6457487a8a5fb3
omar <osahtout@hotmail.com>
2020-12-19 22:51:11 -0500

    refactor (mull-ui): update subheader in homepage
    Updated the sub header in the homepage to not be scrollable and have a background
    
    no refs

--- apps/mull-ui/src/app/components/subnavigation-bar/subnavigation-bar.tsx
+++ apps/mull-ui/src/app/components/subnavigation-bar/subnavigation-bar.tsx
@@ -6,30 +6,32 @@ import './subnavigation-bar.scss';
 
 export const SubNavigationBar = () => {
   return (
-    <div className="subnavigation-container">
-      <NavLink
-        to={ROUTES.DISCOVER}
-        className="subnavigation-link"
-        data-testid="subnavigation-discover-button"
-      >
-        Discover
-      </NavLink>
-      <NavLink
-        to={ROUTES.UPCOMING}
-        className="subnavigation-link"
-        activeClassName="active"
-        data-testid="subnavigation-upcoming-button"
-      >
-        Upcoming
-      </NavLink>
-      <NavLink
-        to={ROUTES.MY_EVENTS}
-        className="subnavigation-link"
-        activeClassName="active"
-        data-testid="subnavigation-myEvents-button"
-      >
-        My Events
-      </NavLink>
+    <div className="subnavigation-header-container">
+      <div className="subnavigation-text-container">
+        <NavLink
+          to={ROUTES.DISCOVER}
+          className="subnavigation-link"
+          data-testid="subnavigation-discover-button"
+        >
+          Discover
+        </NavLink>
+        <NavLink
+          to={ROUTES.UPCOMING}
+          className="subnavigation-link"
+          activeClassName="active"
+          data-testid="subnavigation-upcoming-button"
+        >
+          Upcoming
+        </NavLink>
+        <NavLink
+          to={ROUTES.MY_EVENTS}
+          className="subnavigation-link"
+          activeClassName="active"
+          data-testid="subnavigation-myEvents-button"
+        >
+          My Events
+        </NavLink>
+      </div>
     </div>
   );
 };
