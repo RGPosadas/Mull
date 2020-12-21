f9894e1d9e09dec2dee95f12e0f73cc4d5275e34
Ragith Saba <ragith.coolkid@gmail.com>
2020-12-21 01:05:05 -0500

    test(mull-ui): update tests to use the new start and end date in the dummy event
    Refs #65

--- apps/mull-ui/src/app/app.spec.tsx
+++ apps/mull-ui/src/app/app.spec.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
-import { BrowserRouter } from 'react-router-dom';
-
 import App from './app';
+
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
 
 describe('App', () => {
   it('should render successfully', () => {
     const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <MockedProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </MockedProvider>
     );
 
     expect(baseElement).toBeTruthy();
