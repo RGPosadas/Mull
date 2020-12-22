25558eb2e3b2bb4e96c9bf50c770653a3bec75cd
leojrsilao <leojrsilao@gmail.com>
2020-12-22 12:33:50 -0500

    feat: Add route to events/id when clicking on event card

--- apps/mull-ui/src/app/pages/discover/discover.tsx
+++ apps/mull-ui/src/app/pages/discover/discover.tsx
@@ -4,10 +4,15 @@ import { EventCard } from '../../components';
 import { dummyEvent } from '../../../constants';
 
 import './discover.scss';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 
 export const DiscoverPage = () => {
   const events: [IEvent] = Array(5).fill(dummyEvent) as [IEvent]; // will be replaced with a query call
-  const eventCards = events.map((event, index) => <EventCard key={index} event={event} />);
+  const eventCards = events.map((event, index) => (
+    <Link to={`/events/${event.id}`} key={index}>
+      <EventCard key={index} event={event} />
+    </Link>
+  ));
   return <div className="discover-page-container">{eventCards}</div>;
 };
 
