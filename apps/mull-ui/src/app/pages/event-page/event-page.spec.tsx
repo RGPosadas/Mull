035db27d11eb749ced6c732f0a7f33cd52f7ad79
Cristian Aldea <aldea.cristian@outlook.com>
2020-12-31 17:41:25 -0500

    fix(tests): Add mocked provider, router and route to event page tests

--- apps/mull-ui/src/app/pages/event-page/event-page.spec.tsx
+++ apps/mull-ui/src/app/pages/event-page/event-page.spec.tsx
@@ -4,16 +4,35 @@ import renderer from 'react-test-renderer';
 
 import EventPage from './event-page';
 import { dummyEvent } from '../../../constants';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { Router } from 'express';
+import { MockedProvider } from '@apollo/client/testing';
 
 describe('EventPage', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<EventPage event={dummyEvent} prevPage="" eventImageURL="" />);
+    const { baseElement } = render(
+      <MockedProvider>
+        <MemoryRouter initialEntries={['events/1']}>
+          <Route path="events/:id">
+            <EventPage event={dummyEvent} prevPage="" eventImageURL="" />
+          </Route>
+        </MemoryRouter>
+      </MockedProvider>
+    );
     expect(baseElement).toBeTruthy();
   });
 
   it('should match snapshot', () => {
     const tree = renderer
-      .create(<EventPage event={dummyEvent} prevPage="" eventImageURL="" />)
+      .create(
+        <MockedProvider>
+          <MemoryRouter initialEntries={['events/1']}>
+            <Route path="events/:id">
+              <EventPage event={dummyEvent} prevPage="" eventImageURL="" />
+            </Route>
+          </MemoryRouter>
+        </MockedProvider>
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
