8151dad0746c4bc8541a9bdadee21568e9aeb1d3
leojrsilao <leojrsilao@gmail.com>
2021-01-15 18:40:23 -0500

    feat(mull-api): update snapshot tests for events Refs #66

--- apps/mull-ui/src/app/components/event-card/event-card.spec.tsx
+++ apps/mull-ui/src/app/components/event-card/event-card.spec.tsx
@@ -1,22 +1,46 @@
+import { MockedProvider } from '@apollo/client/testing';
 import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import React from 'react';
+import { Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import { dummyEvent } from '../../../constants';
 import EventCard from './event-card';
 
 describe('EventCard', () => {
+  const history = createMemoryHistory();
   it('should render successfully', () => {
-    const { baseElement } = render(<EventCard event={dummyEvent} />);
+    const { baseElement } = render(
+      <MockedProvider>
+        <Router history={history}>
+          <EventCard event={dummyEvent} />
+        </Router>
+      </MockedProvider>
+    );
     expect(baseElement).toBeTruthy();
   });
 
   it('should match snapshot', () => {
-    const tree = renderer.create(<EventCard event={dummyEvent} />).toJSON();
+    const tree = renderer
+      .create(
+        <MockedProvider>
+          <Router history={history}>
+            <EventCard event={dummyEvent} />
+          </Router>
+        </MockedProvider>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('should render dates correctly', () => {
-    const dom = render(<EventCard event={dummyEvent} />);
+    const dom = render(
+      <MockedProvider>
+        <Router history={history}>
+          <EventCard event={dummyEvent} />
+        </Router>
+      </MockedProvider>
+    );
 
     const dateDiv = dom.getByTestId('event-card-datetime');
     expect(dateDiv.textContent).toBe('15 JUN01:45PM');
--- apps/mull-ui/src/app/components/event-page-info/event-page-info.spec.tsx
+++ apps/mull-ui/src/app/components/event-page-info/event-page-info.spec.tsx
@@ -1,17 +1,35 @@
+import { MockedProvider } from '@apollo/client/testing';
 import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import React from 'react';
+import { Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import { dummyEvent } from '../../../constants';
 import EventPageInfo from './event-page-info';
 
 describe('EventPageInfo', () => {
+  const history = createMemoryHistory();
   it('should render successfully', () => {
-    const { baseElement } = render(<EventPageInfo event={dummyEvent} />);
+    const { baseElement } = render(
+      <MockedProvider>
+        <Router history={history}>
+          <EventPageInfo isJoined={true} isReview={false} event={dummyEvent} />
+        </Router>
+      </MockedProvider>
+    );
     expect(baseElement).toBeTruthy();
   });
 
   it('should match snapshot', () => {
-    const tree = renderer.create(<EventPageInfo event={dummyEvent} />).toJSON();
+    const tree = renderer
+      .create(
+        <MockedProvider>
+          <Router history={history}>
+            <EventPageInfo isJoined={true} isReview={false} event={dummyEvent} />
+          </Router>
+        </MockedProvider>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
