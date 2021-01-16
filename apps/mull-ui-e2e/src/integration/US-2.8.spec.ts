3b33ce7289792f438a8e55434d8bd46f85262e8d
leojrsilao <leojrsilao@gmail.com>
2021-01-15 20:40:19 -0500

    feat(mull-ui-e2e): add e2e tests for joining event on discovver page and preview page of event Refs #66

---
+++ apps/mull-ui-e2e/src/integration/US-2.8.spec.ts
@@ -0,0 +1,21 @@
+/// <reference types="Cypress" />
+import { frameSizes } from './../fixtures/frame-sizes';
+
+frameSizes.forEach((frame) => {
+  describe(`US-2.8: Joining Events (${frame.name} view)`, () => {
+    beforeEach(() => {
+      cy.viewport(frame.res[0], frame.res[1]);
+    });
+
+    it('should join an event on the discover event page', () => {
+      cy.visit('http://localhost:4200/home/discover');
+      cy.get('#event-card-join').click();
+    });
+
+    it('should join an event on the preview event page', () => {
+      cy.visit('http://localhost:4200/home/discover');
+      cy.get(':nth-child(1) > .event-card-image').click();
+      cy.get('[data-testid=mull-button]').click();
+    });
+  });
+});
