e83a49cccde93164a9ea0f067cb60153165e24fe
Jonathan Hsu <bit172@gmail.com>
2021-01-22 18:43:08 -0500

    test(e2e): add e2e test for login
    Ref: #99
    
    Co-authored-by: Jimmy Lau <jimmy71471@gmail.com>
    Co-authored-by: Leo Jr. Silao <leojrsilao@gmail.com>

---
+++ apps/mull-ui-e2e/src/integration/US-7.6.spec.ts
@@ -0,0 +1,42 @@
+/// <reference types="Cypress" />
+import 'cypress-file-upload';
+import { geolocationStub } from '../fixtures';
+import { frameSizes } from './../fixtures/frame-sizes';
+
+frameSizes.forEach((frame) => {
+  describe(`US-7.6: Login as a User (${frame.name} view)`, () => {
+    beforeEach(() => {
+      cy.viewport(frame.res[0], frame.res[1]);
+      cy.visit('http://localhost:4200/', geolocationStub);
+    });
+
+    it('should login a user', () => {
+      cy.get('#email').type('test@email.com');
+      cy.get('#password').type('fdsgsfdj');
+      cy.intercept('POST', 'http://localhost:3333/graphql', (req) => {
+        if (req.body.operationName === 'Login') {
+          req.reply({
+            statusCode: 200,
+            body: {
+              data: {
+                login: {
+                  accessToken:
+                    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTIsImlhdCI6MTYxMTI4OTc5MywiZXhwIjoxNjExMjkzNDM2LCJqdGkiOiJhMmU5NGRjNS03M2MzLTRmYjMtOGM4NS1lZGJiNDhiODlmY2IifQ.d8UHgMh8HglhAXtGj_Szkoctwa-SKBHFxFXQYn6FYHUs',
+                  __typename: 'LoginResult',
+                },
+              },
+            },
+            delayMs: 1000,
+          });
+        }
+      });
+      cy.mockRefreshRequest();
+      cy.get('.login').click();
+      cy.get('.Toastify__toast.Toastify__toast--success', { timeout: 5000 }).should(
+        'have.css',
+        'background-color',
+        'rgb(39, 176, 154)'
+      );
+    });
+  });
+});
