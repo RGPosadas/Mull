f591f3d7725f5cdf90b54b46050de94b2af3ddd6
Jonathan Hsu <bit172@gmail.com>
2021-01-22 18:04:54 -0500

    fix(mull): made cookies available for client application
    Apollo requires that credentials be set to include to be able to accept cookies
    
    Ref: #99

--- apps/mull-ui/src/main.tsx
+++ apps/mull-ui/src/main.tsx
@@ -31,6 +31,7 @@ const authLink = setContext((_, { headers }) => {
   const token = getAccessToken();
   // return the headers to the context so httpLink can read them
   return {
+    credentials: 'include',
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : '',
@@ -41,6 +42,7 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({
   link: authLink.concat(uploadLink),
   cache: new InMemoryCache(),
+  credentials: 'include',
   defaultOptions: {
     watchQuery: {
       fetchPolicy: 'network-only',
