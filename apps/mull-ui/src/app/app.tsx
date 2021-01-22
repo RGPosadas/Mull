14967d0ddcf83cbbeb588ccb3651487b4de1d835
Jonathan Hsu <bit172@gmail.com>
2021-01-21 20:47:01 -0500

    feat(app.tsx): apply private routes to application
    Parts of the application should not be accessible when the user is not logged in
    
    Ref: #99

--- apps/mull-ui/src/app/app.tsx
+++ apps/mull-ui/src/app/app.tsx
@@ -6,6 +6,7 @@ import 'react-toastify/dist/ReactToastify.min.css';
 import { ROUTES } from '../constants';
 import './app.scss';
 import { BotNavBar, SubNavBar, TopNavBar } from './components';
+import PrivateRoute from './components/private-route/private-route';
 import { UserProvider } from './context/user.context';
 import CreateEventPage from './pages/create-event/create-event';
 import EventPage from './pages/event-page/event-page';
@@ -43,20 +44,16 @@ export const App = () => {
     <UserProvider value={{ userId, setUserId, accessToken, setAccessToken }}>
       <div>
         {/* Redirects to discover tab when user is home */}
-        <Route exact path={['/', ROUTES.HOME]}>
+        <PrivateRoute exact path={['/', ROUTES.HOME]}>
           {<Redirect to={ROUTES.DISCOVER} />}
-        </Route>
+        </PrivateRoute>
 
         {/* Switch for main page */}
         <Switch>
           <Route exact path={ROUTES.LOGIN} component={LoginPage} />
           <Route exact path={ROUTES.REGISTER} component={RegisterPage} />
           <Route exact path={ROUTES.TOKEN_REDIRECT} component={TokenRedirectPage} />
-
-          {/* If accessToken isn't set, redirect user to login page. Disabled for now as it will break e2e tests, will be implemented properly in US-7.6 */}
-          {/* {!accessToken ? <Redirect to={ROUTES.LOGIN} /> : null} */}
-
-          <Route exact path={ROUTES.CREATE_EVENT} component={CreateEventPage} />
+          <PrivateRoute exact path={ROUTES.CREATE_EVENT} component={CreateEventPage} />
           <Route
             exact
             path={ROUTES.EVENT_BY_ID}
@@ -68,19 +65,19 @@ export const App = () => {
               />
             }
           />
-          <Route path={ROUTES.HOME}>
+          <PrivateRoute path={ROUTES.HOME}>
             <SubNavBar className="top-nav-bar-shadow" />
             <div className="page-container with-sub-nav-bar">
               <SwipeableRoutes>
-                <Route path={ROUTES.DISCOVER} component={DiscoverPage} />
-                <Route path={ROUTES.UPCOMING} component={UpcomingPage} />
-                <Route path={ROUTES.MY_EVENTS} component={MyEventsPage} />
-                <Route exact path={ROUTES.HOME}>
+                <PrivateRoute path={ROUTES.DISCOVER} component={DiscoverPage} />
+                <PrivateRoute path={ROUTES.UPCOMING} component={UpcomingPage} />
+                <PrivateRoute path={ROUTES.MY_EVENTS} component={MyEventsPage} />
+                <PrivateRoute exact path={ROUTES.HOME}>
                   <Redirect to={ROUTES.DISCOVER} />
-                </Route>
+                </PrivateRoute>
               </SwipeableRoutes>
             </div>
-          </Route>
+          </PrivateRoute>
         </Switch>
 
         <ToastContainer
