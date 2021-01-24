ec98d162e4cd3be6ddf26df89ff415c82d8ced46
Jonathan Hsu <bit172@gmail.com>
2021-01-24 16:36:42 -0500

    style(not-found.tsx): center 404 page content
    Ref: #99

--- apps/mull-ui/src/app/pages/404/not-found.tsx
+++ apps/mull-ui/src/app/pages/404/not-found.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './not-found.scss';
 
 const NotFoundPage = () => {
   return (
-    <div className="page-container">
+    <div className="page-container not-found">
       <p>404 page not found.</p>
     </div>
   );
