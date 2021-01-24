6d485cd788962ce4277ae75aab71f27f8276acd1
Jonathan Hsu <bit172@gmail.com>
2021-01-24 16:31:05 -0500

    test(mull-ui): fix and cleanup failing tests
    Ref: #99

--- apps/mull-ui/src/app/components/private-route/private-route.spec.tsx
+++ apps/mull-ui/src/app/components/private-route/private-route.spec.tsx
@@ -1,10 +1,21 @@
 import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { UserProvider } from '../../context/user.context';
+import NotFoundPage from '../../pages/404/not-found';
 import PrivateRoute from './private-route';
 
 describe('PrivateRoute', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<PrivateRoute />);
+    const history = createMemoryHistory();
+    const { baseElement } = render(
+      <Router history={history}>
+        <UserProvider value={{ userId: 1, setUserId: jest.fn() }}>
+          <PrivateRoute component={NotFoundPage} />
+        </UserProvider>
+      </Router>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
