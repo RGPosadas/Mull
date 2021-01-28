e6a3347729bab56f6a6e4d612801f2dcc130a421
Ritchelle Grace Posadas <ritchelle.posadas@gmail.com>
2021-01-28 01:27:15 -0500

    test(e2e): fix failing e2e tests
    Refs #124

--- apps/mull-ui-e2e/src/integration/US-1.8.spec.ts
+++ apps/mull-ui-e2e/src/integration/US-1.8.spec.ts
@@ -37,7 +37,7 @@ frameSizes.forEach((frame) => {
         `${Cypress.moment().format('D MMM')}03:20 PM`
       );
       cy.get('[data-testid=event-host]').should('have.text', 'Placeholder Host');
-      cy.get('[data-testid=event-location]').should('have.text', 'Test Location');
+      cy.get('[data-testid=event-location]').should('have.text', 'Current Location');
       cy.get('[data-testid=expandable-text-div]').should('have.text', 'test description');
       cy.get('[data-testid=event-restriction]').should('have.text', 'Friends');
     });
@@ -76,7 +76,7 @@ frameSizes.forEach((frame) => {
         `${Cypress.moment().format('D MMM')}03:20 PM`
       );
       cy.get('[data-testid=event-host]').should('have.text', 'Placeholder Host');
-      cy.get('[data-testid=event-location]').should('have.text', 'Test Location');
+      cy.get('[data-testid=event-location]').should('have.text', 'Current Location');
       cy.get('[data-testid=expandable-text-div]').should('have.text', 'new test description');
       cy.get('[data-testid=event-restriction]').should('have.text', 'Everyone');
     });
