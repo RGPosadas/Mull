4b4990b1abec8f635531be0eab22d311bd7fd96c
Ragith Saba <ragith.coolkid@gmail.com>
2021-02-04 01:21:25 -0500

    feat: add auth guard
    Refs #169

--- apps/mull-api/src/app/auth/auth.guard.ts
+++ apps/mull-api/src/app/auth/auth.guard.ts
@@ -1,8 +1,28 @@
 import { GqlContext, IAuthToken } from '@mull/types';
-import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  createParamDecorator,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import jwt from 'jsonwebtoken';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const ctx = GqlExecutionContext.create(context);
+    const accessToken = ctx.getContext<GqlContext>().req.headers.authorization.split(' ')[1];
+    try {
+      return !!jwt.verify(accessToken, environment.jwt.accessSecret);
+    } catch (err) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+  }
+}
 
 export const AuthenticatedUser = createParamDecorator((_, context: ExecutionContext): number => {
   const ctx = GqlExecutionContext.create(context);
