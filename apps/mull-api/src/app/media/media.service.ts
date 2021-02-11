357367128d5d9be910ab485f379277635d58eaa4
Ritchelle Grace Posadas <ritchelle.posadas@gmail.com>
2021-02-11 08:17:36 -0500

    chore: move updateFile above its helper functions
    Refs #189

--- apps/mull-api/src/app/media/media.service.ts
+++ apps/mull-api/src/app/media/media.service.ts
@@ -26,6 +26,27 @@ export class MediaService {
     return media;
   }
 
+  /**
+   * Delete the old file and upload new file (while keeping the same mediaId)
+   * @param newFile new file to upload
+   * @param oldFile old file to delete
+   */
+  async updateFile(newFile: FileUpload, oldFile: MediaInput): Promise<Media> {
+    const mediaIdToKeep = oldFile.id;
+    try {
+      this.deleteStoredFile(oldFile);
+      await this.saveFile(newFile);
+      const newFileType = newFile.mimetype.split('/')[1];
+      this.updateFilename(newFile.filename, mediaIdToKeep, newFileType);
+      if (oldFile.mediaType != newFileType) {
+        await this.mediaRepository.update(mediaIdToKeep, { mediaType: newFileType });
+      }
+    } catch (err) {
+      throw new InternalServerErrorException();
+    }
+    return this.getMedia(mediaIdToKeep);
+  }
+
   saveFile({ createReadStream, filename }: FileUpload): Promise<boolean> {
     return new Promise((resolve, reject) => {
       createReadStream().pipe(
@@ -59,26 +80,5 @@ export class MediaService {
   deleteStoredFile(media: MediaInput): boolean {
     unlinkSync(join(process.cwd(), `/apps/mull-api/uploads/${media.id}.${media.mediaType}`));
     return true;
-  }
-
-  /**
-   * Delete the old file and upload new file (while keeping the same mediaId)
-   * @param newFile new file to upload
-   * @param oldFile old file to delete
-   */
-  async updateFile(newFile: FileUpload, oldFile: MediaInput): Promise<Media> {
-    const mediaIdToKeep = oldFile.id;
-    try {
-      this.deleteStoredFile(oldFile);
-      await this.saveFile(newFile);
-      const newFileType = newFile.mimetype.split('/')[1];
-      this.updateFilename(newFile.filename, mediaIdToKeep, newFileType);
-      if (oldFile.mediaType != newFileType) {
-        await this.mediaRepository.update(mediaIdToKeep, { mediaType: newFileType });
-      }
-    } catch (err) {
-      throw new InternalServerErrorException();
-    }
-    return this.getMedia(mediaIdToKeep);
   }
 }
