24f0d7c871668c4718dde17ad6cfcb96d0e76370
Cristian Aldea <aldea.cristian@outlook.com>
2021-02-13 18:10:10 -0500

    feat(ml): Implement video feed and detection loop
    ref #33

--- apps/mull-ui/src/app/pages/trash-recognition-page/trash-recognition-page.tsx
+++ apps/mull-ui/src/app/pages/trash-recognition-page/trash-recognition-page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { MULL_MODEL_URL } from '../../../constants';
 import { drawDetectionIcons } from '../../../utilities';
 import { TensorflowJsModel } from '../../services/tfjs.model';
@@ -8,43 +8,85 @@ import './trash-recognition-page.scss';
 export interface TrashRecognitionPageProps {}
 
 export function TrashRecognitionPage(props: TrashRecognitionPageProps) {
-  const model = useRef<TensorflowJsModel>(null);
+  const modelRef = useRef<TensorflowJsModel>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const imgRef = useRef<HTMLImageElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-  const width = 500;
-  const height = 667;
+  const [videoLoading, setVideoLoading] = useState<boolean>(true);
+  const [modelLoading, setModelLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    model.current = TensorflowJsModel.getInstance();
-    model.current.init(MULL_MODEL_URL);
-    imgRef.current.setAttribute('crossOrigin', 'anonymous');
+    setup();
   }, []);
 
-  const handleOnClick = async () => {
-    const results = await model.current.detect(imgRef.current, {
-      threshold: 0.45,
-      numResults: 100,
+  const setupCamera = async () => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      let cameraPromise = navigator.mediaDevices
+        .getUserMedia({
+          video: { facingMode: 'user' },
+          audio: false,
+        })
+        .then((stream) => {
+          videoRef.current.srcObject = stream;
+
+          let { width, height } = stream.getTracks()[0].getSettings();
+
+          videoRef.current.height = height;
+          videoRef.current.width = width;
+          canvasRef.current.height = height;
+          canvasRef.current.width = width;
+
+          videoRef.current.play();
+
+          return new Promise<MediaStream>((resolve, reject) => {
+            videoRef.current.onloadedmetadata = () => {
+              setVideoLoading(false);
+              resolve(stream);
+            };
+          });
+        });
+      return cameraPromise;
+    }
+  };
+
+  const setup = async () => {
+    modelRef.current = TensorflowJsModel.getInstance();
+
+    const cameraPromise = setupCamera();
+    const modelPromise = modelRef.current.init(MULL_MODEL_URL);
+
+    await Promise.all([cameraPromise, modelPromise]);
+
+    detectFrame(videoRef.current, modelRef.current, canvasRef.current);
+  };
+
+  const detectFrame = async (
+    video: HTMLVideoElement,
+    model: TensorflowJsModel,
+    canvas: HTMLCanvasElement
+  ) => {
+    const results = await model.detect(video, { numResults: 1, threshold: 0.4 });
+    if (modelLoading) {
+      setModelLoading(false);
+    }
+    drawDetectionIcons(canvas, results, true);
+    requestAnimationFrame(() => {
+      detectFrame(video, model, canvas);
     });
-    const ctx = canvasRef.current.getContext('2d');
-    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-    drawDetectionIcons(ctx, results);
   };
 
   return (
     <div className="page-container">
       <h1>Welcome to trash-recognition-page!</h1>
-      <img
-        alt="Test"
-        ref={imgRef}
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Apple_pie.jpg/1200px-Apple_pie.jpg"
-        width={width}
+      {videoLoading ? <div> Loading Video Feed</div> : null}
+      {modelLoading ? <div> Loading Model</div> : null}
+      <video
+        ref={videoRef}
+        src="https://farm4.staticflickr.com/7372/9303908257_374934b32f_o.jpg"
         className="trash-recognition-overlap"
       />
-      <canvas ref={canvasRef} width={width} height={height} className="trash-recognition-overlap" />
-      <button data-testid="temp-trash-recog-page-button" onClick={handleOnClick}>
-        Click me to detect!
-      </button>
+
+      <canvas ref={canvasRef} className="trash-recognition-overlap" />
     </div>
   );
 }
