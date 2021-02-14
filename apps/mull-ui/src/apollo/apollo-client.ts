94e408e34921a48fa729a63e4057669d83b4e9e3
Jonathan Hsu <bit172@gmail.com>
2021-02-14 15:55:59 -0500

    feat(mull-ui): add automated token refresh upon expiration
    Ref: #170

--- apps/mull-ui/src/apollo/apollo-client.ts
+++ apps/mull-ui/src/apollo/apollo-client.ts
@@ -1,27 +1,77 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, Observable } from '@apollo/client';
+import { IAuthToken } from '@mull/types';
+import { TokenRefreshLink } from 'apollo-link-token-refresh';
 import { createUploadLink } from 'apollo-upload-client';
-import { getAccessToken } from '../app/access-token';
+import jwtDecode from 'jwt-decode';
+import { getAccessToken, setAccessToken } from '../app/access-token';
 import { environment } from '../environments/environment';
+
+const tokenRefreshLink = new TokenRefreshLink({
+  accessTokenField: 'accessToken',
+  isTokenValidOrUndefined: () => {
+    const token = getAccessToken();
+    if (!token) return true;
+    try {
+      const { exp }: IAuthToken = jwtDecode(token);
+      if (Date.now() >= exp * 1000) return false;
+      else return true;
+    } catch {
+      return false;
+    }
+  },
+  fetchAccessToken: () => {
+    return fetch(`${environment.backendUrl}/api/auth/refresh`, {
+      method: 'POST',
+      credentials: 'include',
+    });
+  },
+  handleFetch: (accessToken) => {
+    setAccessToken(accessToken);
+  },
+  handleError: (err) => {
+    console.warn('Your refresh token is invalid. Try to relogin');
+    console.error(err);
+  },
+});
+
+const authLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable((observer) => {
+      let handle: ZenObservable.Subscription;
+      Promise.resolve(operation)
+        .then((operation) => {
+          const accessToken = getAccessToken();
+          if (accessToken) {
+            operation.setContext({
+              headers: {
+                authorization: `Bearer ${accessToken}`,
+              },
+            });
+          }
+        })
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer),
+          });
+        })
+        .catch(observer.error.bind(observer));
+
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+);
+
+const httpLink = new HttpLink({ uri: `${environment.backendUrl}/graphql`, credentials: 'include' });
 
 const uploadLink = createUploadLink({
   uri: `${environment.backendUrl}/graphql`,
 });
 
-const authLink = setContext((_, { headers }) => {
-  const token = getAccessToken();
-  // return the headers to the context so httpLink can read them
-  return {
-    credentials: 'include',
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
 const apolloClient = new ApolloClient({
-  link: authLink.concat(uploadLink),
+  link: ApolloLink.from([tokenRefreshLink, authLink, httpLink, uploadLink]),
   cache: new InMemoryCache(),
   credentials: 'include',
   defaultOptions: {
