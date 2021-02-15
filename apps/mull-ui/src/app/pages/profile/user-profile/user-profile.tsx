c4c1acee8dbd65bb7ed674400ea8d564f1f00e10
Cindy <lo_cindy@outlook.com>
2021-02-15 12:35:42 -0500

    feat(user-profile): add clause for showing friend request count or not
    Refs #188

--- apps/mull-ui/src/app/pages/profile/user-profile/user-profile.tsx
+++ apps/mull-ui/src/app/pages/profile/user-profile/user-profile.tsx
@@ -17,8 +17,9 @@ export interface userProfileProps {
 }
 
 export const UserProfilePage = ({ joinDate, friendRequestCount }: userProfileProps) => {
+  const friendRequestExists = true;
   return (
-    <div>
+    <div className="page-container">
       <ProfileHeader
         userName="Andrea Gloria"
         userPicture="https://blog.photofeeler.com/wp-content/uploads/2017/04/are-bumble-profiles-fake-how-many.jpeg"
@@ -26,23 +27,25 @@ export const UserProfilePage = ({ joinDate, friendRequestCount }: userProfilePro
         userFriends={24}
         userHosting={2}
         userDescription="Regardless of making complicated reasons or calculations, I just want to live simply counting up to about 5 or 6."
-      ></ProfileHeader>
+      />
       <div className="settings-container">
         <h3>Portfolio</h3>
-        <SettingsButton icon={faLeaf} settingName="My Portfolio"></SettingsButton>
+        <SettingsButton icon={faLeaf} settingName="My Portfolio" />
       </div>
       <div className="settings-container with-friends">
         <h3>Friends</h3>
-        <SettingsButton icon={faUserPlus} settingName="Add Friends"></SettingsButton>
-        <SettingsButton icon={faUserFriends} settingName="My Friends"></SettingsButton>
-        <p className="friend-request-count">{(friendRequestCount = 4)}</p>
+        <SettingsButton icon={faUserPlus} settingName="Add Friends" />
+        <SettingsButton icon={faUserFriends} settingName="My Friends" />
+        <p className={friendRequestExists ? 'friend-request-count' : 'no-friend-request'}>
+          {(friendRequestCount = 4)}
+        </p>
       </div>
       <div className="settings-container below-friends">
         <h3>Misc.</h3>
         <Link to="/profile/edit">
-          <SettingsButton icon={faPencilAlt} settingName="Edit Profile"></SettingsButton>
+          <SettingsButton icon={faPencilAlt} settingName="Edit Profile" />
         </Link>
-        <SettingsButton icon={faCog} settingName="Settings"></SettingsButton>
+        <SettingsButton icon={faCog} settingName="Settings" />
       </div>
       <div className="joined-date-container">
         <p>Joined Müll on {(joinDate = 'February 2, 2021')}</p>
