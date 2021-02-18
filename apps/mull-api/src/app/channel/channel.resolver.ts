54eac8f23e9767dd8e91b03b06700960c7a9a15f
leojrsilao <leojrsilao@gmail.com>
2021-02-17 23:39:05 -0500

    feat(mull-api): add channel functionality (add, get and delete) Refs #210
    Co-authored-by: Omar <osahtout@hotmail.com>

--- apps/mull-api/src/app/channel/channel.resolver.ts
+++ apps/mull-api/src/app/channel/channel.resolver.ts
@@ -1,4 +1,31 @@
-import { Resolver } from '@nestjs/graphql';
-
+import { UseGuards } from '@nestjs/common';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthGuard } from '../auth/auth.guard';
+import { Channel } from '../entities';
+import { ChannelService } from './channel.service';
+import { CreateChannelInput } from './inputs/channel.input';
 @Resolver('Channel')
-export class ChannelResolver {}
+export class ChannelResolver {
+  constructor(private readonly channelService: ChannelService) {}
+
+  @Mutation(/* istanbul ignore next */ () => Boolean)
+  @UseGuards(AuthGuard)
+  async createChannel(
+    @Args('input') input: CreateChannelInput,
+    @Args('eventId', { type: /* istanbul ignore next */ () => Int }) eventId?: number
+  ) {
+    return this.channelService.createChannel(input, eventId);
+  }
+
+  @Mutation(/* istanbul ignore next */ () => Boolean)
+  @UseGuards(AuthGuard)
+  async deleteChannel(@Args('id', { type: /* istanbul ignore next */ () => Int }) id: number) {
+    return this.channelService.deleteChannel(id);
+  }
+
+  @Query(/* istanbul ignore next */ () => Channel)
+  @UseGuards(AuthGuard)
+  async getChannel(@Args('id', { type: /* istanbul ignore next */ () => Int }) id: number) {
+    return this.channelService.getChannel(id);
+  }
+}
--- apps/mull-api/src/app/channel/channel.service.ts
+++ apps/mull-api/src/app/channel/channel.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Channel } from '../entities';
+import { CreateChannelInput } from './inputs/channel.input';
 @Injectable()
-export class ChannelService {}
+export class ChannelService {
+  constructor(
+    @InjectRepository(Channel)
+    private channelRepository: Repository<Channel>
+  ) {}
+
+  getChannel(channelId: number): Promise<Channel> {
+    return this.channelRepository.findOne(channelId);
+  }
+
+  async createChannel(input: CreateChannelInput, eventId?: number): Promise<boolean> {
+    await this.channelRepository.save({ ...input, event: { id: eventId } });
+    return true;
+  }
+
+  async deleteChannel(channelId: number): Promise<boolean> {
+    await this.channelRepository.delete(channelId);
+    return true;
+  }
+}
