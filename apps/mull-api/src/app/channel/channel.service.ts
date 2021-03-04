4b9ea6a478132d4aa9b8033da72b725da67d684b
Ritchelle Grace Posadas <ritchelle.posadas@gmail.com>
2021-03-03 20:58:51 -0500

    feat(channel.service): add findUsersInDmChannel function to search for participating users in a DM channel
    - Add eventChannel and dmChannel repositories as well as their respective get/create ops
    
    Refs #247
    
    Co-authored-by: Jonathan <bit172@gmail.com>

--- apps/mull-api/src/app/channel/channel.service.ts
+++ apps/mull-api/src/app/channel/channel.service.ts
@@ -1,13 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Channel } from '../entities';
-import { CreateChannelInput } from './inputs/channel.input';
+import { Channel, DirectMessageChannel, EventChannel } from '../entities';
+import { CreateDmChannelInput, CreateEventChannelInput } from './inputs/channel.input';
 @Injectable()
 export class ChannelService {
   constructor(
     @InjectRepository(Channel)
-    private channelRepository: Repository<Channel>
+    private channelRepository: Repository<Channel>,
+    @InjectRepository(EventChannel)
+    private eventChannelRepository: Repository<EventChannel>,
+    @InjectRepository(DirectMessageChannel)
+    private dmChannelRepository: Repository<DirectMessageChannel>
   ) {}
 
   validateEventChannelWritePermission(channel: Channel, userId: number) {
@@ -64,13 +68,42 @@ export class ChannelService {
     }
   }
 
-  async createChannel(input: CreateChannelInput, eventId?: number): Promise<boolean> {
-    await this.channelRepository.save({ ...input, event: { id: eventId } });
+  getEventChannel(channelId: number): Promise<EventChannel> {
+    return this.eventChannelRepository.findOne(channelId);
+  }
+
+  getDmChannel(channelId: number): Promise<DirectMessageChannel> {
+    return this.dmChannelRepository.findOne(channelId, { relations: ['participants'] });
+  }
+
+  async createEventChannel(input: CreateEventChannelInput, eventId: number): Promise<boolean> {
+    await this.eventChannelRepository.save({ ...input, event: { id: eventId } });
     return true;
+  }
+
+  async createDmChannel(input: CreateDmChannelInput): Promise<DirectMessageChannel> {
+    return await this.dmChannelRepository.save(input);
   }
 
   async deleteChannel(channelId: number): Promise<boolean> {
     await this.channelRepository.delete(channelId);
     return true;
+  }
+
+  async findUsersInDmChannel(userId1: number, userId2: number): Promise<DirectMessageChannel> {
+    const dmChannelsList = await this.dmChannelRepository
+      .createQueryBuilder()
+      .leftJoinAndSelect('DirectMessageChannel.participants', 'participants')
+      .where('participants.id = :userId1 OR participants.id = :userId2', { userId1, userId2 })
+      .getMany();
+
+    // TODO: redo this filtering as TypeORM syntax
+    return dmChannelsList.find(({ participants }) => {
+      let count = 0;
+      for (const participant of participants) {
+        if (participant.id === userId1 || participant.id === userId2) count++;
+      }
+      return count === 2;
+    });
   }
 }
