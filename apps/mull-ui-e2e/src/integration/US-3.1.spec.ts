097e73a4c3ab5d1f9160271103be0844c030b20e
Ragith Saba <ragith.coolkid@gmail.com>
2021-03-04 15:21:23 -0500

    test: add us-3.1 e2e tests
    Refs #156

---
+++ apps/mull-ui-e2e/src/integration/US-3.1.spec.ts
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+import { frameSizes } from './../fixtures/frame-sizes';
+
+frameSizes.forEach((frame) => {
+  describe(`US-3.1: Announcements Page (${frame.name} view)`, () => {
+    beforeEach(() => {
+      cy.interceptGraphQLReqs();
+      cy.viewport(frame.res[0], frame.res[1]);
+    });
+
+    it('should post announcement on announcements page', () => {
+      cy.mockRefreshRequest();
+      const date = new Date();
+      cy.visit('http://localhost:4200/messages/announcements');
+      cy.get('.custom-text-input').type(`${date.toString()}{enter}`);
+      cy.wait(500); // Test was flaky with aliases
+      cy.contains(date.toString());
+    });
+
+    it('should not have access to announcements page', () => {
+      const fakeUserId = -999;
+      cy.mockRefreshRequest(fakeUserId);
+      cy.visit('http://localhost:4200/messages/announcements');
+      cy.get('.announcement-page').should('not.exist');
+    });
+  });
+});
