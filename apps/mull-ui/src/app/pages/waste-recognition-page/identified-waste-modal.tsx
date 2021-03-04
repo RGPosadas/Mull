414102d594f0d77284315afa62969edbeba3e97f
Cindy <lo_cindy@outlook.com>
2021-03-04 00:21:53 -0500

    test(identified-waste-modal): add new test to the modal
    Refs #229

--- apps/mull-ui/src/app/pages/waste-recognition-page/identified-waste-modal.tsx
+++ apps/mull-ui/src/app/pages/waste-recognition-page/identified-waste-modal.tsx
@@ -1,8 +1,8 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Dialog } from '@material-ui/core';
-import { svgMap } from 'apps/mull-ui/src/utilities';
 import React from 'react';
+import { svgMap } from '../../../utilities';
 import './identified-waste-page.scss';
 
 export interface IdentifiedWasteModalProps {
@@ -21,7 +21,7 @@ export const IdentifiedWasteModal = ({
   wasteIcon = '../../.' + svgMap[1];
   wasteTitle = 'This is recyclable!';
   wastePicture = 'http://pm1.narvii.com/5951/2922186bdf76edeb36250994eb99f1c32f31aea9_00.jpg';
-  wasteInfo = 'Needs to be cleaned before recycling. Otherwise it goes to trashbin. ';
+  wasteInfo = 'Needs to be cleaned before recycling. Otherwise it goes to trashbin.';
 
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => {
@@ -33,7 +33,7 @@ export const IdentifiedWasteModal = ({
 
   return (
     <div className="identified-waste-container">
-      <div onClick={handleOpen}>
+      <div onClick={handleOpen} data-testid="open-modal">
         <button>CLICK HERE</button>
       </div>
       <Dialog open={open} onClose={handleClose}>
@@ -41,11 +41,11 @@ export const IdentifiedWasteModal = ({
           <button>{<FontAwesomeIcon icon={faTimes} size="2x" color="grey" />}</button>
         </div>
         <div className="waste-title">
-          <img src={wasteIcon} />
+          <img src={wasteIcon} alt="waste-icon" />
           <h1>{wasteTitle}</h1>
         </div>
         <div className="waste-picture-container">
-          <img src={wastePicture} className="waste-picture" />
+          <img src={wastePicture} className="waste-picture" alt="selected-waste" />
         </div>
         <div className="waste-info"> {wasteInfo}</div>
       </Dialog>
