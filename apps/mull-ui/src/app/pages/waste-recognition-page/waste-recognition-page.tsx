22bfc728e52a877c5eeffaf65839e67083b87f3b
Cristian Aldea <aldea.cristian@outlook.com>
2021-03-10 12:34:13 -0500

    feat(ml): Implement click feature for ml page
    A click listener was setup for the canvas in the waste recognition page, which triggers the onCanvasClick method. The click coordinates are the transformed to image space and the results founds since the last successful detection are looped through to find objects boxes which contain the click. The first result is considered, since results are ordered by confidence.
    
    ref #230

--- apps/mull-ui/src/app/pages/waste-recognition-page/waste-recognition-page.tsx
+++ apps/mull-ui/src/app/pages/waste-recognition-page/waste-recognition-page.tsx
@@ -1,7 +1,8 @@
+import { DetectionResult } from '@mull/types';
 import React, { useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { MULL_MODEL_URL } from '../../../constants';
-import { drawDetectionIcons } from '../../../utilities';
+import { canvasToImageCoords, coordsInBox, drawDetectionIcons } from '../../../utilities';
 import { useToast } from '../../hooks/useToast';
 import { TensorflowJsModel } from '../../services/tfjs.model';
 import IdentifiedWasteModal from './identified-waste-modal';
@@ -15,21 +16,52 @@ export function WasteRecognitionPage(props: WasteRecognitionPageProps) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
   const streamRef = useRef<MediaStream>(null);
-  const { notifyToast, updateToast } = useToast();
+  const resultRef = useRef<DetectionResult[]>([]);
+  const { notifyToast } = useToast();
 
   const [videoLoading, setVideoLoading] = useState<boolean>(true);
   const [modelLoading, setModelLoading] = useState<boolean>(true);
 
   useEffect(() => {
     setup();
+
     return () => {
-      modelRef.current.dispose();
-      if (streamRef.current && streamRef.current.getTracks().length > 0) {
-        streamRef.current.getTracks()[0].stop();
-      }
+      shutdown();
     };
     /* eslint-disable react-hooks/exhaustive-deps */
   }, []);
+
+  /**
+   * Set up model, camera, listeners and other resources for the waste recognition page
+   */
+  const setup = async () => {
+    canvasRef.current.addEventListener('click', onCanvasClick);
+
+    modelRef.current = TensorflowJsModel.getInstance();
+
+    try {
+      await setupCamera();
+    } catch (err) {
+      if (err instanceof DOMException) {
+        notifyToast(`Permission to access the camera was denied`, toast.TYPE.ERROR);
+      } else if (err instanceof Error) {
+        notifyToast(`${err.message}`, toast.TYPE.ERROR);
+      } else {
+        notifyToast(err, toast.TYPE.ERROR);
+      }
+      return;
+    }
+
+    try {
+      await modelRef.current.init(MULL_MODEL_URL);
+    } catch {
+      notifyToast('Detection model loading failed', toast.TYPE.ERROR);
+      setModelLoading(false);
+      return;
+    }
+
+    detectFrame(videoRef.current, modelRef.current, canvasRef.current);
+  };
 
   const setupCamera = async () => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -66,31 +98,15 @@ export function WasteRecognitionPage(props: WasteRecognitionPageProps) {
     }
   };
 
-  const setup = async () => {
-    modelRef.current = TensorflowJsModel.getInstance();
-
-    try {
-      await setupCamera();
-    } catch (err) {
-      if (err instanceof DOMException) {
-        notifyToast(`Permission to access the camera was denied`, toast.TYPE.ERROR);
-      } else if (err instanceof Error) {
-        notifyToast(`${err.message}`, toast.TYPE.ERROR);
-      } else {
-        notifyToast(err, toast.TYPE.ERROR);
-      }
-      return;
+  /**
+   * Shutdown and dispose of page resources, such as the camera, model and listeners
+   */
+  const shutdown = () => {
+    canvasRef.current.removeEventListener('click', onCanvasClick);
+    modelRef.current.dispose();
+    if (streamRef.current && streamRef.current.getTracks().length > 0) {
+      streamRef.current.getTracks()[0].stop();
     }
-
-    try {
-      await modelRef.current.init(MULL_MODEL_URL);
-    } catch {
-      notifyToast('Detection model loading failed', toast.TYPE.ERROR);
-      setModelLoading(false);
-      return;
-    }
-
-    detectFrame(videoRef.current, modelRef.current, canvasRef.current);
   };
 
   const detectFrame = async (
@@ -99,14 +115,44 @@ export function WasteRecognitionPage(props: WasteRecognitionPageProps) {
     canvas: HTMLCanvasElement
   ) => {
     // TODO adjust numResults/threshold to filter out bad results better. Might need to be dynamic
-    const results = await model.detect(video, { numResults: 10, threshold: 0.45 });
+    resultRef.current = await model.detect(video, { numResults: 10, threshold: 0.45 });
     if (modelLoading) {
       setModelLoading(false);
     }
-    drawDetectionIcons(canvas, results);
+    drawDetectionIcons(canvas, resultRef.current);
     requestAnimationFrame(() => {
       detectFrame(video, model, canvas);
     });
+  };
+
+  const onCanvasClick = (event: MouseEvent) => {
+    const canvasCoords = {
+      x: event.offsetX,
+      y: event.offsetY,
+    };
+    const canvasSize = {
+      width: canvasRef.current.clientWidth,
+      height: canvasRef.current.clientHeight,
+    };
+    const imageSize = {
+      width: canvasRef.current.width,
+      height: canvasRef.current.height,
+    };
+    const imageCoords = canvasToImageCoords(canvasCoords, canvasSize, imageSize);
+
+    let clickedObjects = resultRef.current.filter((result) =>
+      coordsInBox(imageCoords, result.bndBox)
+    );
+
+    if (clickedObjects.length > 0) {
+      // Since results are ordered by confidence, we should take the first result to be the one the user wanted to click
+      const clickedObject = clickedObjects[0];
+
+      // TODO, show modal with first result in US-5.2
+      console.log(`Clicked on "${clickedObject.class}"`);
+    } else {
+      console.log('Clicked on nothing');
+    }
   };
 
   return (
