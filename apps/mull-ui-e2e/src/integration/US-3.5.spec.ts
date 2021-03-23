137592d9082032a20b5c65cc77bc0a96cb8b39f3
Omar <osahtout@hotmail.com>
2021-03-23 17:59:41 -0400

    test (mull-ui e2e): US 3.5 e2e test for posting images
    refs #228

---
+++ apps/mull-ui-e2e/src/integration/US-3.5.spec.ts
@@ -0,0 +1,23 @@
+/// <reference types="Cypress" />
+import 'cypress-file-upload';
+import { frameSizes } from './../fixtures/frame-sizes';
+
+frameSizes.forEach((frame) => {
+  describe(`US-3.5: Group Chat Page (${frame.name} view)`, () => {
+    beforeEach(() => {
+      cy.viewport(frame.res[0], frame.res[1]);
+    });
+
+    it('should post an image on the group chat page', () => {
+      const text = 'Test with image';
+      cy.mockRefreshRequest();
+      cy.visit('http://localhost:4200/messages/group-chat');
+      cy.get('.event-chat', { timeout: 5000 }).should('exist');
+
+      cy.get('#imageFile').attachFile('../fixtures/trashed-park.jpg');
+      cy.get('.custom-text-input').type(`${text}{enter}`);
+
+      cy.contains(text);
+    });
+  });
+});
