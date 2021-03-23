fbc4b5903ddd9f6e8790e8dfd2d802ee1a102061
Omar <osahtout@hotmail.com>
2021-03-23 17:47:15 -0400

    feat (mull-ui): preview image
    user can preview the image on the right of the input text before sending it
    
    refs #228

--- apps/mull-ui/src/app/components/chat-input/chat-input.tsx
+++ apps/mull-ui/src/app/components/chat-input/chat-input.tsx
@@ -2,25 +2,33 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IChatForm } from '@mull/types';
 import { FormikContextType } from 'formik';
-import React from 'react';
+import React, { ChangeEvent, MouseEventHandler } from 'react';
 import CustomFileUpload from '../custom-file-upload/custom-file-upload';
 import CustomTextInput from '../custom-text-input/custom-text-input';
 import './chat-input.scss';
 
 export interface ChatInputProps {
   formik: FormikContextType<IChatForm>;
+  image?: string;
+  handleFileUpload?: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleCloseImage?: MouseEventHandler<HTMLImageElement>;
 }
 
-export const ChatInput = ({ formik }: ChatInputProps) => {
+export const ChatInput = ({
+  formik,
+  image,
+  handleFileUpload,
+  handleCloseImage,
+}: ChatInputProps) => {
   return (
     <form onSubmit={formik.handleSubmit} className="chat-input-container" data-testid="chat-input">
-      {/* TODO: Handle Image upload back-end */}
+      {image && <img className="image-size" src={image} alt="" onClick={handleCloseImage} />}
       <CustomFileUpload
         className="file-upload-feedback"
         imageURL={null}
         hasErrors={null}
         errorMessage={null}
-        handleFileUpload={null}
+        handleFileUpload={handleFileUpload}
         fieldName="imageFile"
       />
       <CustomTextInput
