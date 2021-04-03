1733471c7c2e82f7472018f026721783cef09e01
leojrsilao <leojrsilao@gmail.com>
2021-04-03 17:16:17 -0400

    feat(mull-ui): add unit tests for myfriends-profile and add snapshots refs #278
    Co-authored-by: ragith Ragith.coolkid@gmail.com

---
+++ apps/mull-ui/src/app/pages/profile/myFriends-profile/myFriends-profile.spec.tsx
@@ -0,0 +1,69 @@
+import { MockedProvider, MockedResponse } from '@apollo/client/testing';
+import { render } from '@testing-library/react';
+import { GetTrueFriendsDocument, GetTrueFriendsQuery } from 'apps/mull-ui/src/generated/graphql';
+import { createMemoryHistory } from 'history';
+import React from 'react';
+import { Router } from 'react-router-dom';
+import renderer, { act as actRenderer } from 'react-test-renderer';
+import { ROUTES } from '../../../../../src/constants';
+import { UserProvider } from '../../../context/user.context';
+import MyFriends from './myFriends-profile';
+
+describe('MyFriends', () => {
+  const renderHelper = (history, mocks: MockedResponse[]) => {
+    return (
+      <UserProvider value={{ userId: 1, setUserId: jest.fn() }}>
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <Router history={history}>
+            <MyFriends history={history} />
+          </Router>
+        </MockedProvider>
+      </UserProvider>
+    );
+  };
+
+  it('should render successfully', () => {
+    const history = createMemoryHistory();
+    const { baseElement } = render(renderHelper(history, null));
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should match snapshot', async () => {
+    const mocks: MockedResponse[] = [
+      {
+        request: {
+          query: GetTrueFriendsDocument,
+        },
+        result: {
+          data: {
+            getTrueFriends: [
+              {
+                id: 1,
+                name: 'Zoro',
+                avatar: null,
+              },
+              {
+                id: 2,
+                name: 'Luffy',
+                avatar: null,
+              },
+              {
+                id: 3,
+                name: 'Nami',
+                avatar: null,
+              },
+            ],
+          } as GetTrueFriendsQuery,
+        },
+      },
+    ];
+
+    const history = createMemoryHistory();
+    history.push(ROUTES.MY_FRIENDS);
+    await actRenderer(async () => {
+      const tree = renderer.create(renderHelper(history, mocks));
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+  });
+});
