f3e4e261eacf5ecf459b45c5a323234eeb6cc28a
Jonathan Hsu <bit172@gmail.com>
2021-04-05 23:37:15 -0400

    feat(chat-input): disable autocomplete
    Ref #227

--- apps/mull-ui/src/app/components/chat-input/chat-input.tsx
+++ apps/mull-ui/src/app/components/chat-input/chat-input.tsx
@@ -21,37 +21,33 @@ export const ChatInput = ({
   handleCloseImage,
 }: ChatInputProps) => {
   return (
-    <div className="chat-input-container">
-      {formik.touched.imageFile && !!formik.errors.imageFile ? (
-        <span className="error-message">{formik.errors.imageFile}</span>
-      ) : null}
-      <form className="chat-input-form" onSubmit={formik.handleSubmit} data-testid="chat-input">
-        {image && (
-          <img className="chat-input-image-size" src={image} alt="" onClick={handleCloseImage} />
-        )}
-        <CustomFileUpload
-          className="file-upload-feedback"
-          imageURL={null}
-          hasErrors={null}
-          errorMessage={null}
-          handleFileUpload={handleFileUpload}
-          fieldName="imageFile"
-        />
-        <CustomTextInput
-          title=""
-          fieldName="message"
-          value={formik.values.message}
-          onChange={formik.handleChange}
-          hasErrors={null}
-          errorMessage={null}
-          svgIcon={
-            <button type="submit" className="chat-input-button">
-              <FontAwesomeIcon icon={faPaperPlane} className="send-icon" />
-            </button>
-          }
-        />
-      </form>
-    </div>
+    <form onSubmit={formik.handleSubmit} className="chat-input-container" data-testid="chat-input">
+      {image && (
+        <img className="chat-input-image-size" src={image} alt="" onClick={handleCloseImage} />
+      )}
+      <CustomFileUpload
+        className="file-upload-feedback"
+        imageURL={null}
+        hasErrors={null}
+        errorMessage={null}
+        handleFileUpload={handleFileUpload}
+        fieldName="imageFile"
+      />
+      <CustomTextInput
+        title=""
+        fieldName="message"
+        value={formik.values.message}
+        onChange={formik.handleChange}
+        hasErrors={null}
+        errorMessage={null}
+        autoComplete={'off'}
+        svgIcon={
+          <button type="submit" className="chat-input-button">
+            <FontAwesomeIcon icon={faPaperPlane} className="send-icon" />
+          </button>
+        }
+      />
+    </form>
   );
 };
 
