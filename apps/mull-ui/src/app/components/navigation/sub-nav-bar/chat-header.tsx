7097d12cc1b61ec084ed05d2af594c41379b4922
Jonathan Hsu <bit172@gmail.com>
2021-04-06 22:57:38 -0400

    feat(chat-header): add direct message header
    Ref #227

--- apps/mull-ui/src/app/components/navigation/sub-nav-bar/chat-header.tsx
+++ apps/mull-ui/src/app/components/navigation/sub-nav-bar/chat-header.tsx
@@ -1,16 +1,38 @@
 import React, { CSSProperties } from 'react';
+import { getMediaUrl } from '../../../../utilities';
 import './chat-header.scss';
 
 export interface ChatHeaderProps {
   style?: CSSProperties;
   className?: string;
-  eventTitle: string;
+  isDirectMessage?: boolean;
+  userPictureId?: number;
+  userName?: string;
+  eventTitle?: string;
 }
 
-export const ChatHeader = ({ style, className, eventTitle }: ChatHeaderProps) => {
+export const ChatHeader = ({
+  style,
+  className,
+  eventTitle,
+  isDirectMessage,
+  userPictureId,
+  userName,
+}: ChatHeaderProps) => {
   return (
-    <div className={`chat-header ${className ? className : ''}`} style={style}>
-      <h1 className="chat-header-title">{eventTitle}</h1>
+    <div className={`chat-header top-nav-bar-shadow ${className ? className : ''}`} style={style}>
+      {isDirectMessage ? (
+        <div className="direct-message-header-title">
+          <img
+            className="direct-message-header-image"
+            src={getMediaUrl(userPictureId)}
+            alt="user"
+          />
+          <h2 className="direct-message-header-name">{userName}</h2>
+        </div>
+      ) : (
+        <h1 className="chat-header-title">{eventTitle}</h1>
+      )}
     </div>
   );
 };
