3ef4c8d521b30eb6acbe1597ebe4993a15cdb806
Cindy <lo_cindy@outlook.com>
2021-04-08 15:06:16 -0400

    feat(not-found-page): prettify 404 page with a redirect button to homepage
    Refs #321

--- apps/mull-ui/src/app/pages/404/not-found-page.tsx
+++ apps/mull-ui/src/app/pages/404/not-found-page.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../../../../mull-ui/src/constants';
+import { MullButton } from '../../components';
 import './not-found-page.scss';
 
 const NotFoundPage = () => {
   return (
     <div className="page-container not-found">
-      <p>404 page not found.</p>
+      <img
+        src="../../../assets/mull-icon-sad.png"
+        className="sad-mull-icon"
+        alt="sad mull icon"
+      ></img>
+      <p className="not-found-message">
+        Uh oh - this page doesn't exist. <br /> Do you want to go back to the homepage?
+      </p>
+      <Link to={ROUTES.DISCOVER.url}>
+        <MullButton>Go to Homepage</MullButton>
+      </Link>
     </div>
   );
 };
